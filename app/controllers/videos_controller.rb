4298b869b8f32855736f51e5096c94452294f644
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-04-16 14:50:42 -0400

    Adding feature to add a new youtube video link

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,4 +3,13 @@ class VideosController < ApplicationController
 	def index
 		@videos = Video.all
 	end
+
+	def create
+		render :text => "Saving a YouTube video: #{params[:url]}, Title: #{params[:title]}, Publisher: #{params[:publisher]}, Source: #{params[:source]}"
+	end
+
+	def new
+
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get 'videos' => 'videos#index'
+
+  post 'videos' => 'videos#create'
+  get 'videos/new' => 'videos#new'
 end

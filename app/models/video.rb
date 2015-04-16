3bcf58ca0b74f5d8239183d5c965766ded977c46
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-04-16 14:30:24 -0400

    Making the youtube videos work - thanks batman(chris)

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,11 @@
 class Video < ActiveRecord::Base
+
+	def youtube_id
+		if url =~ /https?:\/\/(?:(?:www\.)?youtube\.com\/watch\?v=|youtu\.be\/)(.+)/
+			$1
+		else
+			raise "Cannot find youtube id in #{url}"
+		end
+	end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Video.create!(
+  :publisher  => "TALEGAS619",
+  :title 			=> "Justice Live 2006",
+  :url    		=> "https://youtu.be/4W1S7lYo_1M",
+  :source			=> "YouTube"
+)
+
+Video.create!(
+  :publisher  => "Record Makers",
+  :title 			=> "Kavinsky - Nightcall",
+  :url    		=> "https://youtu.be/MV_3Dpw-BRY",
+  :source			=> "YouTube"
+)
+
+Video.create!(
+  :publisher  => "Drake Vevo",
+  :title 			=> "Drake - Started from the bottom",
+  :url    		=> "https://youtu.be/RubBzkZzpUA",
+  :source			=> "YouTube"
+)

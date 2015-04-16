e74c3d113259017eb7365313b7db7b371e5bc126
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-04-16 15:03:03 -0400

    Refactoring to save new pictures in database

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -5,11 +5,21 @@ class VideosController < ApplicationController
 	end
 
 	def create
-		render :text => "Saving a YouTube video: #{params[:url]}, Title: #{params[:title]}, Publisher: #{params[:publisher]}, Source: #{params[:source]}"
+		@videos = Video.new(video_params)
+		if @videos.save
+			redirect_to videos_url
+		else
+			render :new
+		end
 	end
 
 	def new
+		@videos = Video.new
+	end
 
+	private
+	def video_params
+		params.require(:video).permit(:publisher, :title, :url, :source)
 	end
 
 end
